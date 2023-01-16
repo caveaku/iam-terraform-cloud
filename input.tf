3685813c689cb61eea62342d844c3bede7259b1a
cchi18 <85325018+cchi18@users.noreply.github.com>
2023-01-16 13:08:07 -0500

    Update input.tf

--- input.tf
+++ input.tf
@@ -7,10 +7,10 @@
 #    default           = []
 #   description = "List of users to be created in the google workspace"
 #   type    = list(object({
-#     primary_email = {},
-#      family_name = {},
-#      give_name = {},
-#     disabled = {}
+#     primary_email = string,
+#      family_name = string,
+#      give_name = string,
+#     disabled = bool
   
 #   }))
 # }
