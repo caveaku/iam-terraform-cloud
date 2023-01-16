37aac0c88f65abde0498675cab1584a906576d85
cchi18 <85325018+cchi18@users.noreply.github.com>
2023-01-16 13:08:59 -0500

    Update input.tf

--- input.tf
+++ input.tf
@@ -1,18 +1,18 @@
 
 # variable "region" {}
 
- variable "iam_user_name" {}
+# variable "iam_user_name" {}
 
-# variable "iam_user_name" {
-#    default           = []
-#   description = "List of users to be created in the google workspace"
-#   type    = list(object({
-#     primary_email = string,
-#      family_name = string,
-#      give_name = string,
-#     disabled = bool
+variable "iam_user_name" {
+   default           = []
+  description = "List of users to be created in the google workspace"
+  type    = list(object({
+    primary_email = string,
+     family_name = string,
+     give_name = string,
+    disabled = bool
   
-#   }))
-# }
+  }))
+}
 
 # variable "iam_policy_name" {}
