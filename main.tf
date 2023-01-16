4dbbe9956e35b06cba92c42e4943d5bc4d12a222
cchi18 <85325018+cchi18@users.noreply.github.com>
2023-01-16 12:11:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,35 +2,31 @@ provider "aws" {
    region = var.region   
 }
 
-resource "aws_iam_user" "pascaline" {
-  name = var.user_name    
-  path = "/system/"
-
-  tags = {
-    Name = "iam-user"
-  }
+resource "aws_iam_user" "iam_user" {
+  name = var.iam_user_name
 }
 
-resource "aws_iam_access_key" "pascalin" {
-  user = aws_iam_user.pascalin.name
-}
 
-resource "aws_iam_user_policy" "pascalin" {
-  name = var.iam_policy_name    
-  user = aws_iam_user.pascalin.name
+# resource "aws_iam_access_key" "pascalin" {
+#   user = aws_iam_user.pascalin.name
+# }
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
+# resource "aws_iam_user_policy" "pascalin" {
+#   name = var.iam_policy_name    
+#   user = aws_iam_user.pascalin.name
+
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "ec2:Describe*"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
