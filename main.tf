93810ee84368dacbe5240b6270d43ca0c1a6b1dc
cchi18 <85325018+cchi18@users.noreply.github.com>
2023-01-16 11:52:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ resource "aws_iam_user" "pascaline" {
 }
 
 resource "aws_iam_access_key" "accesskey" {
-  user = aws_iam_user.pascalin.name
+  user = aws_iam_user.pascalin.id
 }
 
 resource "aws_iam_user_policy" "lb_ro" {
   name = var.iam_policy_name    
-  user = aws_iam_user.pascalin.name
+  user = aws_iam_user.pascalin.id
 
   policy = <<EOF
 {
