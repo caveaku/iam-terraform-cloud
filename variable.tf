2483045319e2003016284e44b34263959a336d7e
cchi18 <85325018+cchi18@users.noreply.github.com>
2023-01-16 11:22:27 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,14 @@
+variable "region" {
+  description = "Provide region for the IAM user"
+  type        = string
+}
 
+variable "user_name" {
+  description = "Provide the required name for the newly created IAM user"
+  type        = string
+}
+
+variable "iam_policy_name" {
+  description = "Provide the required name for the newly created IAM policy"
+  type        = string
+}
