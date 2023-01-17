1c22846590458c218f67b8ee00d6653fd59e9f64
cchi18 <85325018+cchi18@users.noreply.github.com>
2023-01-17 14:53:43 -0500

    Update input.tf

--- input.tf
+++ input.tf
@@ -3,10 +3,11 @@
 
 # variable "iam_user_name" {}
 
-variable "user_names" {
-  type = list(string)
-  #default = ["user1", "user2", "user3"]
-}
+
+# variable "user_names" {
+#   type = list(string)
+#   #default = ["user1", "user2", "user3"]
+# }
 
 # variable "user_name_prefix" {}
 
